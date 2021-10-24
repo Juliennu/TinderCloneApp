b38baa4ed12fdd2782b84e710ad252bf71e32375
Juliennu <wsb.1099@gmail.com>
2021-10-24 18:09:13 +0900

    CardView内にLabelを配置

--- TinderApp/Views/CardView.swift
+++ TinderApp/Views/CardView.swift
@@ -11,7 +11,7 @@ class CardView: UIView {
     
     let cardImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .blue
+        imageView.backgroundColor = .gray//.blue
         imageView.layer.cornerRadius = 10
         imageView.contentMode = .scaleToFill
         return imageView
@@ -25,14 +25,70 @@ class CardView: UIView {
         return label
     }()
     
+    let infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        let buttonImage = UIImage(systemName: "info.circle.fill")?
+            //サイズ調整
+            .resize(size: .init(width: 40, height: 40))
+        button.setImage(buttonImage, for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    let residenceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .white
+        label.text = "日本、栃木"
+        return label
+    }()
+    
+    let hobbyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .white
+        label.text = "ボードゲーム"
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    let introductionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .white
+        label.text = "土日に一緒に遊べる友達を探しています"
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-//        backgroundColor = .brown
+        let infoVerticalStackView = UIStackView(arrangedSubviews: [residenceLabel, hobbyLabel, introductionLabel])
+        infoVerticalStackView.axis = .vertical
+
+        
+        let baseStackView = UIStackView(arrangedSubviews: [infoVerticalStackView, infoButton])
+        baseStackView.axis = .horizontal
+        
         addSubview(cardImageView)
         addSubview(nameLabel)
+        addSubview(baseStackView)
+        
         cardImageView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: 10)
-        nameLabel.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, bottomPadding: 40, leftPadding: 20)
+        
+        baseStackView.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, right: cardImageView.rightAnchor, bottomPadding: 20, leftPadding: 20, rightPadding: 20)
+        
+        infoButton.anchor(width: 40)
+        
+        nameLabel.anchor(bottom: baseStackView.topAnchor, left: cardImageView.leftAnchor, bottomPadding: 10, leftPadding: 20)
+    }
+    
+    private func setUpStackView() {
+
     }
     
     required init?(coder: NSCoder) {
